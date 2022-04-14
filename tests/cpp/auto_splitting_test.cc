154c034d2fcee9d9fbc7578b27b70771de4fd301
huajsj <huaj@xilinx.com>
2022-04-13 18:05:36 -0700

    add device mapping information.

--- tests/cpp/auto_splitting_test.cc
+++ tests/cpp/auto_splitting_test.cc
@@ -19,7 +19,7 @@
 
 #include <dmlc/logging.h>
 #include <gtest/gtest.h>
-
+#include <algorithm>
 #include <list>
 #include <unordered_map>
 #include <ostream>
@@ -37,11 +37,33 @@ typedef struct DPItem_ {
     int end;
     float perf = std::numeric_limits<float>::max();
     DevType dev_type = DEV_MAX;
+    std::string GetType() {
+      std::string ret;
+      switch(dev_type) {
+        case CPU:
+          ret = "CPU";
+          break;
+        case GPU:
+          ret = "GPU";
+          break;
+        case FPGA:
+          ret = "FPGA";
+          break;
+      }
+      return ret;
+    }
     friend std::ostream& operator <<(std::ostream& os, struct DPItem_ di) {
-        os << " "<<  di.start << "-" << di.end << " type:" << di.dev_type;
+        os << " "<<  di.start << "-" << di.end << " type:" << di.GetType();
         return os;
     }
 }DPItem;
+
+struct {
+ bool operator()(std::pair<float, std::list<DPItem>>& l,
+            std::pair<float, std::list<DPItem>>& r) const {
+              return l.first < r.first;
+       }
+}comp;
 
 class AutoTune {
     using PERF=std::unordered_map<DevType, float>;
@@ -53,6 +75,7 @@ class AutoTune {
         return weight * perf_assume_[pos] * dev_weight;
     }
     void GenerateGraph(int num) {
+        std::cout << "          CPU       GPU      FPGA" << std::endl;
         for (int i = 0 ; i < num ; i++) {
             int pos = rand()%perf_assume_.size();
             PERF& perf = layer_perf_[i];
@@ -79,15 +102,17 @@ class AutoTune {
                          int prev_pipeline_end_bound, 
                          std::vector<DevType> available_dev, 
                          int network_depth,
-                         std::list<DPItem> &sub_list) {
+                         std::list<DPItem> &sub_list,
+                         std::vector<std::pair<float, std::list<DPItem>>>& perf_list) {
         if (current_pipeline_index >= dev_num || prev_pipeline_end_bound >= network_depth - 1
             || available_dev.size() == 0)  {
             float perf = std::numeric_limits<float>::min();
             for (auto x:sub_list) {
-                std::cout << x ;
+                //std::cout << x ;
                 perf = std::max(perf, GetPerSum(x, lperf));
             }
-            std::cout << "  average perf is " << perf << std::endl;
+            //std::cout << "  average perf is " << perf << std::endl;
+            perf_list.push_back(std::make_pair(perf, sub_list));
             return;
         }
          
@@ -100,14 +125,14 @@ class AutoTune {
                   DPItem di(prev_pipeline_end_bound + 1, i, available_dev[k]);
                   sub_list.push_back(di);
                   GenerateBalance(current_pipeline_index + 1,lperf, dev_num, i,  
-                                  dev_copy, network_depth, sub_list);
+                                  dev_copy, network_depth, sub_list, perf_list);
                   sub_list.pop_back();
               }
             } else {
                 DPItem di(prev_pipeline_end_bound + 1, network_depth - 1, available_dev[k]);
                 sub_list.push_back(di);
                 GenerateBalance(current_pipeline_index + 1, lperf, dev_num, network_depth - 1, 
-                                dev_copy, network_depth, sub_list);
+                                dev_copy, network_depth, sub_list, perf_list);
                 sub_list.pop_back();
             }
        }
@@ -119,10 +144,23 @@ class AutoTune {
             auto sub_graph = split_conf[i].first;
         }
         return true;
-    };
+    }
+    void ShowBest() {
+      std::sort(perf_list.begin(), perf_list.end(), comp);
+      size_t max_list = 10;
+      std::cout << "The top " << max_list << "  best split configure are: " << std::endl;
+      for (int i = 0; i < std::min(perf_list.size(), max_list); i++) {
+          auto item = perf_list[i];
+          for (auto x:item.second) {
+             std::cout << x << " ";
+          }
+          std::cout << " perf is " << item.first << std::endl;
+      }
+    }
     std::unordered_map<int, PERF> layer_perf_;
     /*device weight*/
     PERF dev_={{CPU,1.0}, {GPU,0.20}, {FPGA,0.3}};
+    std::vector<std::pair<float, std::list<DPItem>>> perf_list;
  private:
     static float GetPerSum(DPItem di, std::unordered_map<int, PERF> lperf) {
         DevType  dtype = di.dev_type;
@@ -145,10 +183,11 @@ class AutoTune {
 };
 TEST(AutoSplitting, TVMAutoSplitting) {
     std::cout << "Perf Data !\n";
-    int net_depth = 10;
+    int net_depth = 15;
     std::list<DPItem> list;
     AutoTune at;
     at.GenerateGraph(net_depth);
     AutoTune::GenerateBalance(0, at.layer_perf_,at.dev_.size(), -1,
-                              {CPU, GPU, FPGA}, net_depth, list);
+                              {CPU, GPU, FPGA}, net_depth, list, at.perf_list);
+    at.ShowBest();
 }
