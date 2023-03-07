81c9fbad20aff48bb8fd20c280123dc614fbf349
Hua Jiang <huaj@xsjhuaj50.xilinx.com>
2023-03-06 17:40:06 -0800

    add test.py

---
+++ zendnn/test.py
@@ -0,0 +1,18 @@
+import tvm
+from tvm import relay
+from tvm.relay import testing
+
+data=relay.var("data", relay.TensorType((1,), "float32"))
+const=relay.const(1, "float32")
+net=relay.add(data, const)
+f=relay.Function(relay.analysis.free_vars(net),net)
+m, p=testing.create_workload(f)
+
+@tvm.ir.register_op_attr("add", "target.zendnn")
+def _support(attr):
+    return True
+
+pm=relay.transform.AnnotateTarget("zendnn")(m)
+mod=relay.transform.PartitionGraph()(pm)
+
+lib=relay.build(mod, "llvm")
